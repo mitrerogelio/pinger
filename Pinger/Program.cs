a61a0357bdddb73af4612534c7dc6033db1b705d
Rogelio Mitre <mitrerogelio@outlook.com>
2023-01-31 22:31:02 -0600

    first commit

---
+++ Pinger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.NetworkInformation;
+using System.Text;
+
+// Pinging Google DNS Server
+
+Ping pingSender = new Ping();
+PingOptions options = new PingOptions();
+
+options.DontFragment = true;
+
+String data = "Learn to code";
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+int timeout = 120;
+string address = "4.2.2.2";
+PingReply reply = pingSender.Send(address, timeout, buffer, options);
+if (reply.Status == IPStatus.Success)
+{
+    Console.WriteLine($"Response: {reply.Status.ToString()}");
+    Console.WriteLine($"RoundTrip: {reply.RoundtripTime}");
+    Console.WriteLine($"Time to Live: {reply.Options.Ttl}");
+    Console.WriteLine($"Buffer Size: {reply.Buffer.Length}");
+}
